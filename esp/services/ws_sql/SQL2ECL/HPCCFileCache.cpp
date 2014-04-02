1622b292a8e15574a7bc578cdb91e831c7e8cd0b
rpastrana <rodrigo.pastrana@lexisnexis.com>
2014-04-02 11:10:31 -0400

    WSSQL-2 handle exceptions while fetching hpcc file info
    Signed-off-by: rpastrana <rodrigo.pastrana@lexisnexis.com>

--- esp/services/ws_sql/SQL2ECL/HPCCFileCache.cpp
+++ esp/services/ws_sql/SQL2ECL/HPCCFileCache.cpp
@@ -122,87 +122,116 @@ bool HPCCFileCache::cacheHpccFileByName(const char * filename)
     if(cache.getValue(filename))
         return true;
 
-    IDistributedFileDirectory & dfd = queryDistributedFileDirectory();
-    Owned<IDistributedFile> df = dfd.lookup(filename, userdesc);
+    Owned<HPCCFile> file;
 
-    if(!df)
-        throw MakeStringException(-1,"Cannot find file %s.",filename);
-
-    Owned<HPCCFile> file =  HPCCFile::createHPCCFile();
-    const char* lname=df->queryLogicalName(), *fname=strrchr(lname,':');
-    file->setFullname(lname);
-    file->setName(fname ? fname+1 : lname);
-
-    //Do we care about the clusters??
-    //StringArray clusters;
-    //if (cluster && *cluster)
-    //{
-    //df->getClusterNames(clusters);
-    //if(!FindInStringArray(clusters, cluster))
-        //throw MakeStringException(ECLWATCH_FILE_NOT_EXIST,"Cannot find file %s.",fname);
-    //}
-
-#if defined(_DEBUG)
-    StringBuffer atttree;
-    toXML(&df->queryAttributes(), atttree, 0);
-    fprintf(stderr, "%s", atttree.str());
-#endif
-
-    IPropertyTree & properties = df->queryAttributes();
-
-    if(properties.hasProp("ECL"))
-        file->setEcl(properties.queryProp("ECL"));
-
-    //unfortunately @format sometimes holds the file format, sometimes @kind does
-    const char * kind = properties.queryProp("@kind");
-    if (kind)
+    try
     {
-        file->setFormat(kind);
-        if ((stricmp(kind, "key") == 0))
+        IDistributedFileDirectory & dfd = queryDistributedFileDirectory();
+        Owned<IDistributedFile> df = dfd.lookup(filename, userdesc);
+
+        if(!df)
+            throw MakeStringException(-1,"Cannot find file %s.",filename);
+
+        file.setown(HPCCFile::createHPCCFile());
+        const char* lname=df->queryLogicalName(), *fname=strrchr(lname,':');
+        file->setFullname(lname);
+        file->setName(fname ? fname+1 : lname);
+
+        //Do we care about the clusters??
+        //StringArray clusters;
+        //if (cluster && *cluster)
+        //{
+        //df->getClusterNames(clusters);
+        //if(!FindInStringArray(clusters, cluster))
+            //throw MakeStringException(ECLWATCH_FILE_NOT_EXIST,"Cannot find file %s.",fname);
+        //}
+
+    #if defined(_DEBUG)
+        StringBuffer atttree;
+        toXML(&df->queryAttributes(), atttree, 0);
+        fprintf(stderr, "%s", atttree.str());
+    #endif
+
+        IPropertyTree & properties = df->queryAttributes();
+
+        if(properties.hasProp("ECL"))
+            file->setEcl(properties.queryProp("ECL"));
+
+        //unfortunately @format sometimes holds the file format, sometimes @kind does
+        const char * kind = properties.queryProp("@kind");
+        if (kind)
         {
-            file->setIsKeyedFile(true);
-
-            ISecManager *secmgr = NULL;
-            ISecUser *secuser = NULL;
-            StringBuffer username;
-            StringBuffer passwd;
-            userdesc->getUserName(username);
-            userdesc->getPassword(passwd);
-
-            Owned<IResultSetFactory> resultSetFactory = getSecResultSetFactory(secmgr, secuser, username.str(), passwd.str());
-            Owned<INewResultSet> result;
-            result.setown(resultSetFactory->createNewFileResultSet(filename, NULL));
-
-            Owned<IResultSetCursor> cursor = result->createCursor();
-            const IResultSetMetaData & meta = cursor->queryResultSet()->getMetaData();
-            int columnCount = meta.getColumnCount();
-            int keyedColumnCount = meta.getNumKeyedColumns();
-
-            for (int i = 0; i < keyedColumnCount; i++)
+            file->setFormat(kind);
+            if ((stricmp(kind, "key") == 0))
             {
-                SCMStringBuffer columnLabel;
-                if (meta.hasSetTranslation(i))
-                {
-                    meta.getNaturalColumnLabel(columnLabel, i);
-                }
+                file->setIsKeyedFile(true);
 
-                if (columnLabel.length() < 1)
-                {
-                    meta.getColumnLabel(columnLabel, i);
-                }
+                ISecManager *secmgr = NULL;
+                ISecUser *secuser = NULL;
+                StringBuffer username;
+                StringBuffer passwd;
+                userdesc->getUserName(username);
+                userdesc->getPassword(passwd);
 
-                file->setKeyedColumn(columnLabel.str());
+                Owned<IResultSetFactory> resultSetFactory = getSecResultSetFactory(secmgr, secuser, username.str(), passwd.str());
+                Owned<INewResultSet> result;
+                try
+                {
+                    result.setown(resultSetFactory->createNewFileResultSet(filename, NULL));
+
+                    if (result)
+                    {
+                        Owned<IResultSetCursor> cursor = result->createCursor();
+                        const IResultSetMetaData & meta = cursor->queryResultSet()->getMetaData();
+                        int columnCount = meta.getColumnCount();
+                        int keyedColumnCount = meta.getNumKeyedColumns();
+
+                        DBGLOG("Keyed file %s column count: %d, keyedcolumncount: %d", file->getFullname(), columnCount, keyedColumnCount);
+                        for (int i = 0; i < keyedColumnCount; i++)
+                        {
+                            SCMStringBuffer columnLabel;
+                            if (meta.hasSetTranslation(i))
+                            {
+                                meta.getNaturalColumnLabel(columnLabel, i);
+                            }
+
+                            if (columnLabel.length() < 1)
+                            {
+                                meta.getColumnLabel(columnLabel, i);
+                            }
+
+                            file->setKeyedColumn(columnLabel.str());
+                        }
+                    }
+                }
+                catch (IException * se)
+                {
+                    StringBuffer s;
+                    se->errorMessage(s);
+                    DBGLOG("Error fetching keyed file %s info: %s", fname, s.str());
+                    se->Release();
+                }
             }
         }
+        else
+        {
+            //@format - what format the file is (if not fixed width)
+            const char* format = properties.queryProp("@format");
+            file->setFormat(format);
+        }
     }
-    else
+    catch (IException * se)
     {
-        //@format - what format the file is (if not fixed width)
-        const char* format = properties.queryProp("@format");
-        file->setFormat(format);
+        StringBuffer s;
+        se->errorMessage(s);
+        DBGLOG("Error fetching HPCC file %s info: %s", filename, s.str());
+        se->Release();
     }
 
-    cache.setValue(file->getFullname(), file.getLink());
+    if (file)
+        cache.setValue(file->getFullname(), file.getLink());
+    else
+        cache.setValue(filename, NULL); //avoid attempting to fetch next time
 
     return true;
 }
