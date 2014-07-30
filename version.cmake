62667f1a51c86d4addbcadf84885dd57d16af804
rpastrana <rodrigo.pastrana@lexisnexis.com>
2014-07-30 13:58:15 -0400

    WSSQL-44 Tech preview maturity = rc Signed-off-by: rpastrana <rodrigo.pastrana@lexisnexis.com>

--- version.cmake
+++ version.cmake
@@ -4,6 +4,6 @@
 set ( WSSQL_MAJOR 5 )
 set ( WSSQL_MINOR 0 )
 set ( WSSQL_POINT 0 )
-set ( WSSQL_MATURITY "release" )
-set ( WSSQL_SEQUENCE 2 )
+set ( WSSQL_MATURITY "rc" )
+set ( WSSQL_SEQUENCE 1 )
 ###
