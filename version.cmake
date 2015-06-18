79e02e8c68a348e26d2f5af89e84e75216816459
rpastrana <rodrigo.pastrana@lexisnexis.com>
2015-06-18 10:19:13 -0400

    #INTERNAL version 5.4.0
    Signed-off-by: rpastrana <rodrigo.pastrana@lexisnexis.com>
    
    Conflicts:
    
    	version.cmake

--- version.cmake
+++ version.cmake
@@ -2,8 +2,8 @@
 ## Version Information
 ###
 set ( WSSQL_MAJOR 5 )
-set ( WSSQL_MINOR 2 )
-set ( WSSQL_POINT 4 )
-set ( WSSQL_MATURITY "release" )
+set ( WSSQL_MINOR 4 )
+set ( WSSQL_POINT 0 )
+set ( WSSQL_MATURITY "trunk" )
 set ( WSSQL_SEQUENCE 1 )
 ###
