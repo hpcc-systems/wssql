a2ca3a9425af3e9ab6ad5456458a71a9dd893658
rpastrana <rodrigo.pastrana@lexisnexis.com>
2015-08-13 11:45:27 -0400

    WsSQL Master 5.4.1 trunk
    Signed-off-by: rpastrana <rodrigo.pastrana@lexisnexis.com>

--- version.cmake
+++ version.cmake
@@ -4,7 +4,7 @@
 
 set ( WSSQL_MAJOR 5 )
 set ( WSSQL_MINOR 4 )
-set ( WSSQL_POINT 0 )
-set ( WSSQL_MATURITY "release" )
+set ( WSSQL_POINT 1 )
+set ( WSSQL_MATURITY "trunk" )
 set ( WSSQL_SEQUENCE 1 )
 ###
