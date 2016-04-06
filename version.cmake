5c76b739768c6875b0b0938e162f2f25bc35b5d2
rpastrana <rodrigo.pastrana@lexisnexis.com>
2016-04-06 14:36:26 -0400

    WSSQL Merging candidate 6.0.0 branch
    Signed-off-by: rpastrana <rodrigo.pastrana@lexisnexis.com>

--- version.cmake
+++ version.cmake
@@ -3,8 +3,8 @@
 ###
 
 set ( WSSQL_MAJOR 6 )
-set ( WSSQL_MINOR 0 )
+set ( WSSQL_MINOR 1 )
 set ( WSSQL_POINT 0 )
-set ( WSSQL_MATURITY "release" )
+set ( WSSQL_MATURITY "trunk" )
 set ( WSSQL_SEQUENCE 1 )
 ###
