67c2710221bad91cfee185d83816ca24b9ebe0bf
rpastrana <rodrigo.pastrana@lexisnexis.com>
2016-04-21 11:05:03 -0400

    WSSQL Version update 6.0.0 rc 2
    Signed-off-by: rpastrana <rodrigo.pastrana@lexisnexis.com>

--- version.cmake
+++ version.cmake
@@ -4,7 +4,7 @@
 
 set ( WSSQL_MAJOR 6 )
 set ( WSSQL_MINOR 0 )
-set ( WSSQL_POINT 1 )
-set ( WSSQL_MATURITY "trunk" )
-set ( WSSQL_SEQUENCE 1 )
+set ( WSSQL_POINT 0 )
+set ( WSSQL_MATURITY "rc" )
+set ( WSSQL_SEQUENCE 2 )
 ###
