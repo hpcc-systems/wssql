a69000a8c8a18a3ee17a1b4771e7e196c60965d4
rpastrana <rodrigo.pastrana@lexisnexis.com>
2016-10-10 10:29:50 -0400

    WSSQL Split off 6.2.0 branch
    Signed-off-by: rpastrana <rodrigo.pastrana@lexisnexis.com>

--- version.cmake
+++ version.cmake
@@ -3,8 +3,8 @@
 ###
 
 set ( WSSQL_MAJOR 6 )
-set ( WSSQL_MINOR 1 )
+set ( WSSQL_MINOR 2 )
 set ( WSSQL_POINT 0 )
-set ( WSSQL_MATURITY "trunk" )
+set ( WSSQL_MATURITY "rc" )
 set ( WSSQL_SEQUENCE 1 )
 ###
