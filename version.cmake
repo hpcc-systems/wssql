0c294b67b18758522d0a699a7c1ef05aaf2ed356
Rodrigo Pastrana <rodrigo.pastrana@lexisnexis.com>
2017-02-02 16:38:11 -0500

    WSSQL Branch candidate 6.2.2 rc1
    Signed-off-by: Rodrigo Pastrana <rodrigo.pastrana@lexisnexis.com>

--- version.cmake
+++ version.cmake
@@ -4,7 +4,7 @@
 
 set ( WSSQL_MAJOR 6 )
 set ( WSSQL_MINOR 2 )
-set ( WSSQL_POINT 0 )
-set ( WSSQL_MATURITY "release" )
-set ( WSSQL_SEQUENCE 2 )
+set ( WSSQL_POINT 2 )
+set ( WSSQL_MATURITY "rc" )
+set ( WSSQL_SEQUENCE 1 )
 ###
