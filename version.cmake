8bd80eca335f307c4a4a9a991362148ec255251c
Rodrigo Pastrana <rodrigo.pastrana@lexisnexis.com>
2017-02-06 11:57:09 -0500

    WSSQL Branch candidate master update
    Signed-off-by: Rodrigo Pastrana <rodrigo.pastrana@lexisnexis.com>
    
    # Conflicts:
    #	version.cmake

--- version.cmake
+++ version.cmake
@@ -3,8 +3,8 @@
 ###
 
 set ( WSSQL_MAJOR 6 )
-set ( WSSQL_MINOR 1 )
+set ( WSSQL_MINOR 3 )
 set ( WSSQL_POINT 0 )
 set ( WSSQL_MATURITY "trunk" )
-set ( WSSQL_SEQUENCE 1 )
+set ( WSSQL_SEQUENCE 0 )
 ###
